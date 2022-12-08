6f02f25498ece062254a0c617305fe9de8a0380f
DESKTOP-VMCGTS6\aj755 <aj75531@gmail.com>
2022-12-07 19:12:00 -0600

    dfas

--- customfloatnetwork.py
+++ customfloatnetwork.py
@@ -159,7 +159,7 @@ def fixLayers(model: torch.nn.Module, cfloat:CustomFloat, fixBias = True):
     if not allowLayerClamp:
         return
     on = 1 if cfloat.signed else 0
-    parms = model.named_parameters().cpu()
+    parms = model.to(device=torch.device('cpu')).named_parameters()
     for name, param in parms: 
         if param.requires_grad:
             if("bias" in name and fixBias):
